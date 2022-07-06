4352d03d81b6fc1c3c8e38672143cfb637ce10e6
Eduarda Adriana <106124425+Duarda31@users.noreply.github.com>
2022-07-06 20:13:04 -0300

    Add files via upload

---
+++ exercicio 3/exercicio 3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------------------------------------------------------");
+
+            for (int i = 1; i <= 50; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            string restar4 = Console.ReadLine();
+            if (restar4.ToUpper() == "R")
+            {
+                System.Diagnostics.Process.Start(System.AppDomain.CurrentDomain.FriendlyName);
+
+                Environment.Exit(0);
+            }
+        }
+    }
+}
